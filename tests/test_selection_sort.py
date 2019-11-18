4b84a421be52ff4046e4f483dc800eea1de95010
Tanveer Singh <tanveer.singh@labela.nl>
2019-11-18 14:19:52 +0100

    Add sorting test for selection sort.

--- tests/test_selection_sort.py
+++ tests/test_selection_sort.py
@@ -12,3 +12,7 @@ class TestSelectionSort(TestCase):
         assert self._client.sort(
             self._numeric_unsorted_list
         ) == [2, 9, 11, 12, 24, 32, 54, 834]
+
+    def test_sorted_list_stays_sorted(self):
+        sorted_list = [1, 2, 3]
+        assert self._client.sort(sorted_list) == self._client.sort(sorted_list)
